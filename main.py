1e67e1cd11538a8573916d2bebda7bfeea82c164
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-09-05 17:32:41 -0300

    Várias mudanças. Ver em baixo. (João, Pedro, Thomaz)
    classe inimigos, sistema wsd, dois formatos de tela (1280x720), colisão com a borda e colisão com inimigos.

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import pygame # biblioteca usada pra rodar o jogo
 import sys # biblioteca usada pra fechar o programa
 import time #biblioteca usada para o timer funcionar
+import random #biblioteca usada para randomizar números
 
+class Inimigos:
+    def __init__(self, velocidade):
+        w = pygame.display.get_surface().get_width()
+        h = pygame.display.get_surface().get_height()
+        self.largura = w / (25.6 * 2)
+        self.altura = h / (14.4 * 2)
+        self.velocidade = velocidade
+        self.contador = 0
+
+    def spawnar(self):
+        global retangulo	
+        w = pygame.display.get_surface().get_width()	
+        h = pygame.display.get_surface().get_height()	
+        escolher = False	
+        while not escolher:	
+            valorx = random.randrange(0, w)	
+            valory = random.randrange(0, h)	
+            if (abs(retangulo.x - valorx) ** 2 + abs(retangulo.y - valory)** 2) ** (1/2) >= 300:	
+                self.x = valorx	
+                self.y = valory	
+                escolher = True
+
+    def desenhar_inimigo(self, janela):
+        pygame.draw.rect(janela, vermelho, (self.x, self.y, self.largura, self.altura))
+        
+        
 class Retangulo:
     def __init__(self, x, y, velocidade, stamina):
         w = pygame.display.get_surface().get_width()
@@ -17,19 +44,19 @@ class Retangulo:
         global janela
         #método usado pra conferir qual tecla foi usada mais recentemente
         global setas
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             setas['RIGHT'] += 1
         else:
             setas['RIGHT'] = 0
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             setas['LEFT'] += 1
         else:
             setas['LEFT'] = 0
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             setas['UP'] += 1
         else:
             setas['UP'] = 0
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             setas['DOWN'] += 1
         else:
             setas['DOWN'] = 0
@@ -41,6 +68,7 @@ class Retangulo:
                 if not vezes or setas[seta] <= vezes:
                     vezes = setas[seta]
                     escolhida = seta
+        print(setas)
         
         if keys[pygame.K_F11]:
             janela = pygame.display.set_mode((largura, altura), pygame.FULLSCREEN)
@@ -62,20 +90,21 @@ class Retangulo:
             self.velocidade = 0.4
         if not keys[pygame.K_LSHIFT]:
             self.velocidade = 0.7
-        if keys[pygame.K_LCTRL]:
+        if keys[pygame.K_LCTRL] and escolhida:
             if self.stamina >= 1:
                 self.stamina -= 1
                 self.velocidade = 1.1
-        if not keys[pygame.K_LCTRL]:
+        else:
             if self.stamina < 1000:
                 self.stamina += 0.65
 
-    def desenha(self, janela):
-        w = pygame.display.get_surface().get_width()
-        h = pygame.display.get_surface().get_height()
-        self.largura = w / 25.6
-        self.altura = h / 14.4
-        pygame.draw.rect(janela, verde, (self.x, self.y, self.largura, self.altura))
+    def desenhar_mago(self, janela):
+        width = pygame.display.get_surface().get_width()
+        height = pygame.display.get_surface().get_height()
+        self.largura = width / 25.6
+        self.altura = height / 14.4
+        pygame.draw.rect(janela, verde, (self.x, self.y, self.largura, self.altura)) #tá errado
+
 
 pygame.init()
 
@@ -98,6 +127,36 @@ comeco_timer = time.time() #início do timer
 # Cria o retângulo
 retangulo = Retangulo(100, 100, 0.7, 1000) # x, y, largura, altura, velocidade e stamina
 
+# Spawnar os animais
+for i in range(3):
+  locals()['inimigo' + str(i)] = Inimigos(0.5)
+  locals()['inimigo' + str(i)].spawnar()
+
+# Colisão com as bordas
+def borda(variavel):
+    global width
+    global height
+    if variavel.x < 0:
+        variavel.x = 0
+    if variavel.x > width - variavel.largura:
+        variavel.x = width - variavel.largura
+    if variavel.y < 0:
+        variavel.y = 0
+    if variavel.y > height - variavel.altura:
+        variavel.y = height - variavel.altura
+    return variavel
+
+# Colisão do player com os animais
+def colisao(player, objeto):
+    global width
+    global height
+    #não está funcionando corretamente por enquanto
+    if (player.x <= (objeto.x + objeto.largura) and (player.x + player.largura) >= objeto.largura) or (player.x >= (objeto.largura + objeto.x) and (player.largura + player.x) <= objeto.x):
+        if (player.y <= (objeto.y + objeto.altura) and (player.y + player.altura) >= objeto.altura) or (player.y >= (objeto.altura + objeto.y) and (player.altura + player.y) <= objeto.y):
+            objeto.spawnar()
+            objeto.contador += 1
+    return objeto
+
 setas = {'RIGHT': 0, 'LEFT': 0, 'UP': 0, 'DOWN': 0} # Status de movimento inicial do retângulo (parado)
 # Loop principal
 running = True
@@ -110,7 +169,10 @@ while running:
 
     exibir_janela.fill(branco)
 
-    retangulo.desenha(exibir_janela)
+    retangulo.desenhar_mago(exibir_janela)
+
+    for i in range(3):
+        locals()['inimigo' + str(i)].desenhar_inimigo(exibir_janela)
 
     ratio_stamina = retangulo.stamina / 1000
 
@@ -126,14 +188,11 @@ while running:
     pygame.display.flip()
 
     # Colisão com as bordas
-    if retangulo.x < 0:
-        retangulo.x = 0
-    if retangulo.x > width - retangulo.largura:
-        retangulo.x = width - retangulo.largura
-    if retangulo.y < 0:
-        retangulo.y = 0
-    if retangulo.y > height - retangulo.altura:
-        retangulo.y = height - retangulo.altura
+    retangulo = borda(retangulo)
+    for i in range(3): 
+        vez = locals()['inimigo' + str(i)]
+        vez = borda(vez)
+        vez = colisao(retangulo, vez)
     pygame.display.update()
 
     retangulo.move(pygame.key.get_pressed())
