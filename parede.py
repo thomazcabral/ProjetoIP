4bed2a4d5265ec59cdccfca48a186c180030f993
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-09-13 09:02:36 -0300

    Update parede.py

--- parede.py
+++ parede.py
@@ -1,7 +1,17 @@
 import pygame as pg
 import random
-from .utilidades import *
-from .retangulo import retangulo
+
+
+PRETO = (0, 0, 0)
+LARGURA = 1280
+ALTURA = 720
+janela = pg.display.set_mode((LARGURA, ALTURA))
+
+
+
+def colisao_amigavel(objeto1, objeto2):
+    if (objeto2.x + objeto2.largura >= objeto1.x >= objeto2.x or objeto1.x + objeto1.largura >= objeto2.x >= objeto1.x) and (objeto2.y + objeto2.altura >= objeto1.y >= objeto2.y or objeto1.y + objeto1.altura >= objeto2.y >= objeto1.y):
+        return True
 
 
 class Parede:
@@ -17,7 +27,7 @@ class Parede:
         while not escolher:
             self.x = random.randrange(0, int(w - self.largura))
             self.y = random.randrange(50, int(h - 60 - self.altura))
-            if not colisao_amigavel(self, retangulo):
+            if not colisao_amigavel(self, self.retangulo):
                 escolher = True
             for parede in Parede.paredes:
                 if ((parede.x + (parede.largura / 2) - self.x) ** 2 + (parede.y + (parede.altura / 2) - self.y)** 2) ** (1/2) < parede.raio:
