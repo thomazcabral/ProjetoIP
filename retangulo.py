dfea97dc76f83cb6edb00f041e28b732a248186f
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-09-14 15:02:08 -0300

    Update retangulo.py

--- retangulo.py
+++ retangulo.py
@@ -1,7 +1,5 @@
 import pygame as pg
 
-from .parede import Parede
-
 def colisao_amigavel(objeto1, objeto2):
     if (objeto2.x + objeto2.largura >= objeto1.x >= objeto2.x or objeto1.x + objeto1.largura >= objeto2.x >= objeto1.x) and (objeto2.y + objeto2.altura >= objeto1.y >= objeto2.y or objeto1.y + objeto1.altura >= objeto2.y >= objeto1.y):
         return True
@@ -11,7 +9,7 @@ ALTURA = 720
 stamina_padrao = 1000
 
 class Retangulo:
-    def __init__(self, x, y, velocidade, stamina):
+    def __init__(self, x, y, velocidade, stamina, vida):
         w = pg.display.get_surface().get_width()
         h = pg.display.get_surface().get_height()
         self.x = x
@@ -20,27 +18,43 @@ class Retangulo:
         self.altura = h / 14.4
         self.velocidade = velocidade
         self.stamina = stamina
+        self.vida = vida
         self.cansaco = 0
-        self.img = pg.image.load('mago_down.png')
+        self.img = pg.image.load('assets/mago_down.png')
         self.raio = 300
 
-    def move(self, keys, variacao_tempo, setas):
-        global janela
+    def move(self, keys, variacao_tempo, setas, ultima_seta, paredes, rios):
         #método usado pra conferir qual tecla foi usada mais recentemente
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
             setas['RIGHT'] += 1
+            ultima_seta['RIGHT'] = 1
+            ultima_seta['LEFT'] = 0
+            ultima_seta['UP'] = 0
+            ultima_seta['DOWN'] = 0
         else:
             setas['RIGHT'] = 0
         if keys[pg.K_LEFT] or keys[pg.K_a]:
             setas['LEFT'] += 1
+            ultima_seta['RIGHT'] = 0
+            ultima_seta['LEFT'] = 1
+            ultima_seta['UP'] = 0
+            ultima_seta['DOWN'] = 0
         else:
             setas['LEFT'] = 0
         if keys[pg.K_UP] or keys[pg.K_w]:
             setas['UP'] += 1
+            ultima_seta['RIGHT'] = 0
+            ultima_seta['LEFT'] = 0
+            ultima_seta['UP'] = 1
+            ultima_seta['DOWN'] = 0
         else:
             setas['UP'] = 0
         if keys[pg.K_DOWN] or keys[pg.K_s]:
             setas['DOWN'] += 1
+            ultima_seta['RIGHT'] = 0
+            ultima_seta['LEFT'] = 0
+            ultima_seta['UP'] = 0
+            ultima_seta['DOWN'] = 1
         else:
             setas['DOWN'] = 0
         
@@ -56,22 +70,26 @@ class Retangulo:
         antigo_y = self.y
         if escolhida:
             if escolhida == 'RIGHT':
-                self.img = pg.image.load('mago_right.png')
+                self.img = pg.image.load('assets/mago_right.png')
                 self.x += self.velocidade * variacao_tempo
             elif escolhida == 'LEFT':
-                self.img = pg.image.load('mago_left.png')
+                self.img = pg.image.load('assets/mago_left.png')
                 self.x -= self.velocidade * variacao_tempo
             elif escolhida == 'UP':
-                self.img = pg.image.load('mago_up.png')
+                self.img = pg.image.load('assets/mago_up.png')
                 self.y -= self.velocidade * variacao_tempo
             elif escolhida == 'DOWN':
-                self.img = pg.image.load('mago_down.png')
+                self.img = pg.image.load('assets/mago_down.png')
                 self.y += self.velocidade * variacao_tempo
             else:
-                self.img = pg.image.load('mago_down.png')
-        global Parede
-        for parede in Parede.paredes:
-            if colisao_amigavel(self, parede):
+                self.img = pg.image.load('assets/mago_down.png')
+        bloqueio = []
+        for k in paredes:
+            bloqueio.append(k)
+        for k in rios:
+            bloqueio.append(k)
+        for bloqueador in bloqueio:
+            if colisao_amigavel(self, bloqueador):
                 self.x = antigo_x
                 self.y = antigo_y
             
@@ -93,12 +111,11 @@ class Retangulo:
             self.cansaco -= 1
             
     def desenhar_mago(self, janela):
-        global width
-        global height
         escala = 1/4
-        imagem = pg.image.load('mago_down.png')
+        imagem = pg.image.load('assets/mago_down.png')
         w, h = imagem.get_size()
         self.largura = w * escala
-        self.altura = h * escala
+        # como a imagem do mago é gerada 2/3 abaixo do y dele, a hitbox coincide com sua parte mais inferior
+        self.altura = (h * escala) / 3
         redimensionar = pg.transform.smoothscale(self.img, ((w*escala), (h*escala)))
-        janela.blit(redimensionar, (self.x, self.y))
+        janela.blit(redimensionar, (self.x, self.y - 2 * self.altura))
