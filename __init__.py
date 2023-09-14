5e928c3797f9dec1a758147484f6108b68165e34
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2023-09-13 21:59:45 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 from .inimigos import Inimigos
-from .parede import Parede
-from .retangulo import Retangulo
+from .classes.parede import Parede
+from .classes.retangulo import Retangulo
+from .rio import Rio
+from .lobo import Lobo
