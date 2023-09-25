2edefa0008b7933aac52b9b460c2b4ca129e5a8c
pedroagaeme <futurograduado@gmail.com>
2023-09-25 17:06:41 -0300

    aborts

--- main.py
+++ main.py
@@ -42,10 +42,14 @@ MARROM = (210, 180, 140)
 MARROM_ESCURO = (123, 66, 48)
 CINZA = (211,211,211)
 
+LARGURA_MAPA = 1280 * 2
+ALTURA_MAPA = 720 * 2
 # Configurações da janela
-LARGURA = 1280
-ALTURA = 720
-janela = pg.display.set_mode((LARGURA, ALTURA))
+largura_camera = 1280
+altura_camera = 720
+offset_x = 0
+offset_y = 0
+janela = pg.display.set_mode((largura_camera, altura_camera))
 tela_cheia = False
 
 
@@ -92,8 +96,8 @@ for i in range(num_tiles):
     tilemap.append(pg.transform.scale(pg.image.load(f'assets/tile{i + 1}.png'), (50, 50)))
 for j in range(num_enfeites):
     desenho_enfeites.append(pg.transform.scale(pg.image.load(f'assets/enfeite{j + 1}.png'), (50, 50)))
-for x in range(0, LARGURA, 50):
-    for y in range(0, ALTURA, 50):
+for x in range(0, LARGURA_MAPA, 50):
+    for y in range(0, ALTURA_MAPA, 50):
         aleatorio = random.randint(1,10)
         if aleatorio == 1:
             numero = random.randint(2, 13)
@@ -115,13 +119,13 @@ if ('direita' in fluxo_rio and 'esquerda' in fluxo_rio) or ('cima' in fluxo_rio 
     fluxo_rio.remove(direcao_rio_final)
 
 if direcao_rio_inicial == 'direita':
-    y = random.randrange(0, ALTURA, 100)
+    y = random.randrange(0, ALTURA_MAPA, 100)
     mapa[(0, y)] = mapa[(50, y)] = mapa[(0, y + 50)] = mapa[(50, y + 50)] = 14
     x_vez = 0
     y_vez = y
 elif direcao_rio_inicial == 'esquerda':
-    y = random.randrange(0, ALTURA, 100)
-    ultimo = LARGURA
+    y = random.randrange(0, ALTURA_MAPA, 100)
+    ultimo = LARGURA_MAPA
     while ultimo % 50 != 0:
         ultimo -= 1
     ultimo -= 50
@@ -129,13 +133,13 @@ elif direcao_rio_inicial == 'esquerda':
     x_vez = ultimo
     y_vez = y
 elif direcao_rio_inicial == 'baixo':
-    x = random.randrange(0, LARGURA, 100)
+    x = random.randrange(0, LARGURA_MAPA, 100)
     mapa[(x, 0)] = mapa[(x, 50)] = mapa[(x + 50, 0)] = mapa[(x + 50, 50)] = 14
     x_vez = x
     y_vez = 0
 elif direcao_rio_inicial == 'cima':
-    x = random.randrange(0, LARGURA, 100)
-    ultimo = ALTURA
+    x = random.randrange(0, LARGURA_MAPA, 100)
+    ultimo = ALTURA_MAPA
     while ultimo % 50 != 0:
         ultimo -= 1
     ultimo -= 50
@@ -152,7 +156,7 @@ while not foz:
         direcao_rio = direcao_rio_inicial
     if direcao_rio == 'direita':
         x_vez += 100
-        if x_vez + 150 > LARGURA:
+        if x_vez + 150 > LARGURA_MAPA:
                 foz = True
         mapa[(x_vez, y_vez)] = mapa[(x_vez, y_vez + 50)] = mapa[(x_vez + 50, y_vez)] = mapa[(x_vez + 50, y_vez + 50)] = 14
     elif direcao_rio == 'esquerda':
@@ -162,7 +166,7 @@ while not foz:
         mapa[(x_vez, y_vez)] = mapa[(x_vez, y_vez + 50)] = mapa[(x_vez + 50, y_vez)] = mapa[(x_vez + 50, y_vez + 50)] = 14
     elif direcao_rio == 'baixo':
         y_vez += 100
-        if y_vez + 100 > ALTURA:
+        if y_vez + 100 > ALTURA_MAPA:
             foz = True
         mapa[(x_vez, y_vez)] = mapa[(x_vez, y_vez + 50)] = mapa[(x_vez + 50, y_vez)] = mapa[(x_vez + 50, y_vez + 50)] = 14
     elif direcao_rio == 'cima':
@@ -173,8 +177,8 @@ while not foz:
     functions.contorno_rio(mapa, x_vez, y_vez)
     Rio(x_vez, y_vez)
 
-for x in range(0, LARGURA, 50):
-    for y in range(0, ALTURA, 50):
+for x in range(0, LARGURA_MAPA, 50):
+    for y in range(0, ALTURA_MAPA, 50):
         if mapa[(x, y)] == 1:
             num = random.randint(1, 30)
             if num == 1:
@@ -231,8 +235,18 @@ while running:
     # A movimentação é em função do tempo, se rodar muito ciclos ele para e volta dps
     variacao_tempo = clock.tick(30)
 
-    width = pg.display.get_surface().get_width()
-    height = pg.display.get_surface().get_height()
+    largura_camera = pg.display.get_surface().get_width()
+    offset_x = mago.x - (largura_camera / 2)
+    if offset_x < 0:
+        offset_x = 0
+    elif offset_x + (largura_camera / 2) > LARGURA_MAPA:
+        offset_x = LARGURA_MAPA - largura_camera
+    offset_y = mago.y - (altura_camera / 2)
+    if offset_y < 0:
+        offset_y = 0
+    elif offset_y + (altura_camera / 2) > ALTURA_MAPA:
+        offset_y = ALTURA_MAPA - altura_camera
+    altura_camera = pg.display.get_surface().get_height()
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
@@ -301,20 +315,20 @@ while running:
     keys = pg.key.get_pressed()
 
     if keys[pg.K_F11] and not tela_cheia:
-        janela = pg.display.set_mode((LARGURA, ALTURA), pg.FULLSCREEN)
+        janela = pg.display.set_mode((largura_camera, altura_camera), pg.FULLSCREEN)
         tela_cheia = True
     if keys[pg.K_ESCAPE] and tela_cheia:
-        janela = pg.display.set_mode((LARGURA, ALTURA))
+        janela = pg.display.set_mode((largura_camera, altura_camera))
         tela_cheia = False
 
-    for x in range(0, width, 50):
-        for y in range(0, height, 50):
-            janela.blit(tilemap[mapa[(x, y)] - 1], (x, y))
+    for x in range(0, LARGURA_MAPA, 50):
+        for y in range(0, ALTURA_MAPA, 50):
+            janela.blit(tilemap[mapa[(x, y)] - 1], (x - offset_x, y - offset_y))
     
-    for x in range(0, width, 50):
-        for y in range(0, height, 50):
+    for x in range(0, LARGURA_MAPA, 50):
+        for y in range(0, ALTURA_MAPA, 50):
             if (x, y) in enfeites.keys():
-                janela.blit(desenho_enfeites[enfeites[(x, y)][0] - 1], enfeites[(x, y)][1])
+                janela.blit(desenho_enfeites[enfeites[(x, y)][0] - 1], (enfeites[(x, y)][1][0] - offset_x, enfeites[(x, y)][1][1] - offset_y))
     
 
     for j in range(num_arvores):
@@ -328,7 +342,7 @@ while running:
     total_vivos = len(Animais.animais_vivos)
     nenhum = True
     for animal in Animais.animais_vivos:
-        if not (animal.x < -1 * animal.largura or animal.x >= width or animal.y < -1 * animal.altura or animal.y > height):
+        if not (animal.x < -1 * animal.largura or animal.x >= largura_camera or animal.y < -1 * animal.altura or animal.y > altura_camera):
             nenhum = False
     if nenhum or total_vivos == 0 or (chance == 1 and total_vivos <= 20): 
         nome = random.choice([j for j in infos.keys()])
@@ -350,7 +364,7 @@ while running:
     ratio_vida = mago.vida / 1000
 
     #lugar de informacões
-    janela.blit(hud, (-200, height - 60))
+    janela.blit(hud, (-200, altura_camera - 60))
 
 
     #Moldura barras
@@ -360,10 +374,10 @@ while running:
     altura_barra_habilidade = 48
     raio_borda = 4
     espessura = 2
-    x_barras = width / (LARGURA/10)
-    y_barra_stamina = height / (ALTURA/(ALTURA - 28))
-    y_barra_vida = height / (ALTURA/(ALTURA - 44))
-    x_barra_habilidade = width / (LARGURA / 270)
+    x_barras = largura_camera / (largura_camera/10)
+    y_barra_stamina = altura_camera / (altura_camera/(altura_camera - 28))
+    y_barra_vida = altura_camera / (altura_camera/(altura_camera - 44))
+    x_barra_habilidade = largura_camera / (largura_camera / 270)
     y_barra_habilidade = y_barra_vida - 9
     
     #Fundo barra de stamina
@@ -395,7 +409,7 @@ while running:
     for animal in Animais.animais_vivos:
         animal.move(mago, variacao_tempo, Parede.paredes, Rio.rios, Dragao.dragoes_vivos, velocidade_devagar, velocidade_rapida)
     # Colisão com as bordas
-    mago = functions.borda(mago, width, height)
+    mago = functions.borda(mago, LARGURA_MAPA, ALTURA_MAPA)
     for animal in Animais.animais_vivos: 
         animal = functions.colisao(mago, animal, pontos_animais)
 
@@ -416,20 +430,20 @@ while running:
     tempo_restante = max(0, duracao_timer - tempo_passado) #evite com que o timer dê errado quando acabe
     minutos, segundos = divmod(int(tempo_restante), 60) #faz a divisão correta entre minutos e segundos
     texto_timer = fonte_tempo.render(f'Tempo: {minutos:02d}:{segundos:02d}', True, BRANCO) #texto, situação de aparecimento, cor
-    janela.blit(texto_timer, (LARGURA - texto_timer.get_width() - 15, ALTURA - 50))
-    x_inicial = LARGURA - texto_timer.get_width() - 150
+    janela.blit(texto_timer, (largura_camera - texto_timer.get_width() - 15, altura_camera - 50))
+    x_inicial = largura_camera - texto_timer.get_width() - 150
 
     #Blitando os sprites do animais no contador
-    janela.blit(animal1_baixo, (x_inicial - 200, ALTURA - 50))
-    janela.blit(animal2_baixo, (x_inicial - 100, ALTURA - 50))
-    janela.blit(animal3, (x_inicial, ALTURA - 50))
+    janela.blit(animal1_baixo, (x_inicial - 200, altura_camera - 50))
+    janela.blit(animal2_baixo, (x_inicial - 100, altura_camera - 50))
+    janela.blit(animal3, (x_inicial, altura_camera - 50))
 
     for animal in reversed(pontos_animais.keys()): #contador dos animais
         if pontos_animais[animal] < 10:
             contador = fonte_contador.render(f'x0{pontos_animais[animal]}', True, BRANCO)
         else:
             contador = fonte_contador.render(f'x{pontos_animais[animal]}', True, BRANCO) 
-        janela.blit(contador, (x_inicial + 27, ALTURA - 57))
+        janela.blit(contador, (x_inicial + 27, altura_camera - 57))
         x_inicial -= 100
     
     if not cooldown:
